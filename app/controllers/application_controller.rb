89b552c8711bc54e9d390337ca39223bcdfae3d1
Yauheni Sushkevich <thisislogiiiiin@gmail.com>
2015-12-18 12:13:43 +0300

    CSRF token fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, only: [:registration]
 
   def index
     render text: 'Pushkin'
@@ -24,9 +25,10 @@ class ApplicationController < ActionController::Base
     if @token.save!
       p "TOKEN SAVED"
       logger.info "TOKEN SAVED"
-      answer = level2 question
-      logger.info  answer
     end
+
+    answer = level2 question
+    logger.info  answer
     render json: {answer: answer}
   end
 
