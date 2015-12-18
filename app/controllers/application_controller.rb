fab67a8cbe4b8ead9ba57fd4aa1777910756b274
Yauheni Sushkevich <thisislogiiiiin@gmail.com>
2015-12-18 12:59:03 +0300

    require net/http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+require 'net/http'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token, only: [:registration, :quiz]
-  require 'net/http'
+
 
   def index
     render text: 'Pushkin'
