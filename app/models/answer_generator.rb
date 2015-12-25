25cfa0c34da339390e5d64d6e95e51df471b9b01
Yauheni Sushkevich <thisislogiiiiin@gmail.com>
2015-12-25 22:46:33 +0300

    make level8 faster

--- app/models/answer_generator.rb
+++ app/models/answer_generator.rb
@@ -85,12 +85,14 @@ class AnswerGenerator < ActiveRecord::Base
           duplicate[i] = char
 
           q << @@dictionary[duplicate.chars.sort.join.strip]
-          break unless q.compact!.empty?
+          #binding.pry
+          
+          break unless q.compact.empty?
         end
-        break unless q.empty?
+        break unless q.compact.empty?
       end
     end
-    #q.uniq!.compact!
+    q.uniq!.compact!
     #puts q.join ' '
 
     #binding.pry
