77a720d4b9a1a299c0745fdf1000e9711c5443e4
Yauheni Sushkevich <thisislogiiiiin@gmail.com>
2015-12-27 12:53:37 +0300

    answer - instance var

--- app/models/answer_generator.rb
+++ app/models/answer_generator.rb
@@ -23,7 +23,7 @@ class AnswerGenerator < ActiveRecord::Base
       
       @answer << find_replaced_word_in_poem(text, splited)
       
-      @poem = nil if answer.compact.empty?
+      @poem = nil if @answer.compact.empty?
     end
     @answer.join ','
   end
