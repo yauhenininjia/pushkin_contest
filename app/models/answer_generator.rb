e8bf344f35f06d7958554adfb69aca4aef4adefb
Yauheni Sushkevich <thisislogiiiiin@gmail.com>
2016-01-07 16:03:21 +0300

    add logger for answer array

--- app/models/answer_generator.rb
+++ app/models/answer_generator.rb
@@ -18,6 +18,7 @@ class AnswerGenerator < ActiveRecord::Base
       @answer << find_replaced_word_in_poem(text, splited)
       @poem = nil if @answer.compact.empty?
     end
+    logger.info @answer
     @answer.join ','
   end
 
